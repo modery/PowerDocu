9b3aa259c2a18bd4a5933c551056640eb0cdfcc9
Rene Modery <rene@modery.net>
2021-10-27 20:57:09 +0800

    PowerDocu.GUI initial version
    First version of a Windows GUI

---
+++ PowerDocu.GUI/PowerDocuForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PowerDocu.GUI
+{
+    partial class PowerDocuForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "PowerDocu GUI";
+            openFileDialog1 = new OpenFileDialog()
+            {
+                FileName = "*.zip",
+                Filter = "ZIP files (*.zip)|*.zip",
+                Title = "Open exported Flow ZIP file"
+            };
+
+            selectButton = new Button()
+            {
+                Size = new Size(150, 30),
+                Location = new Point(15, 15),
+                Text = "Select Flow to document"
+            };
+            selectButton.Click += new EventHandler(selectButton_Click);
+            Controls.Add(selectButton);
+            textBox1 = new TextBox
+            {
+                Size = new Size(750, 300),
+                Location = new Point(15, 60),
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+            };
+            Controls.Add(textBox1);
+        }
+
+        private Button selectButton;
+        private OpenFileDialog openFileDialog1;
+        private TextBox textBox1;
+
+        #endregion
+    }
+}
+
---
+++ PowerDocu.GUI/PowerDocuForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using PowerDocu.FlowDocumenter;
+
+namespace PowerDocu.GUI
+{
+    public partial class PowerDocuForm : Form
+    {
+
+        public PowerDocuForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //run FlowParser from here
+                    textBox1.AppendText(FlowDocumentationGenerator.GenerateWordDocumentation(openFileDialog1.FileName));
+                    textBox1.AppendText(Environment.NewLine);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+
+                }
+            }
+        }
+
+    }
+}
