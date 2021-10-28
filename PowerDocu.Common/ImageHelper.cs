27864dcf13467ac8c64609db62c5d223f4b781d1
Rene Modery <rene@modery.net>
2021-10-28 22:56:03 +0800

    Update ImageHelper.cs
    Removing console.writeline that's not needed

--- PowerDocu.Common/ImageHelper.cs
+++ PowerDocu.Common/ImageHelper.cs
@@ -11,7 +11,6 @@ namespace PowerDocu.Common
         public static void ConvertImageTo32(string imagepath, string destinationpath)
         {
             Bitmap bmp = new Bitmap(imagepath);
-            Console.WriteLine((int)(32 * (bmp.Height / bmp.Width)));
             Bitmap resized = new Bitmap(bmp, new Size(32, (int)(32 * (bmp.Height / bmp.Width))));
             resized.Save(destinationpath, ImageFormat.Png);
             resized.Dispose();
