462be5468eef082a1f105ebaa0aa9fd5332e7865
Rene Modery <rene@modery.net>
2021-12-13 10:39:42 +0800

    Improved connector parsing and updated list (2021-12-13)
    Instead of the manual scraping of a page, we are now using a page that returns all connector infos as JSON. Easy to parse now!
    
    Updated the connectors list accordingly as of 13 Dec 2021

--- PowerDocu.Common/Resources/ConnectorIcons/GenerateConnectorMapping.ps1
+++ PowerDocu.Common/Resources/ConnectorIcons/GenerateConnectorMapping.ps1
@@ -1,42 +1,26 @@
 # Run this file to generate a current list of connector icon URLs and have them downloaded
 
 
-$myURI = "https://flow.microsoft.com/en-us/connectors/"
-
-$searchClass = "connector-item"
-
-# items have the following structure:
-#
-#<a title="Harness PDFx" href="/en-us/connectors/shared_harnesspdfx/harness-pdfx/" data-event-name="ApiBrowsed" data-automation-id="connector-link" data-event-apidisplayname="Harness PDFx" data-event-apiname="shared_harnesspdfx">
-#<img aria-hidden="true" onerror="this.onerror = null; this.src = 'https://psuxasia.azureedge.net/Content/retail/assets/connection.668c984c82d6c33195b0c32a5f48ea34.2.svg'" alt="Harness PDFx" src="https://connectoricons-prod.azureedge.net/releases/v1.0.1507/1.0.1507.2528/harnesspdfx/icon.png">
-#<p class="api-name">Harness PDFx</p>
-#<div class="premium-label-container" ng-non-bindable="">
-#<span class="premium-label">
-#PREMIUM
-#</span>
-#</div>
-#</a>
+$myURI = "https://powerautomate.microsoft.com/en-us/api/connectors/all/"
 
 $connectors = @()
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $req = Invoke-Webrequest -URI $myURI
-$parsedconnectors = $req.ParsedHtml.getElementsByClassName($searchClass)
-write-host "$($parsedconnectors.length) potential connectors found. Starting processing"
-$parsedconnectors| %{
-    #lots of manual parsing here. Does its job at the moment, could be improved if needed
-    $nameextract = $_.innerHTML.substring($_.innerHTML.IndexOf('title="')+7)
-    $name = $nameextract.substring(0,$nameextract.IndexOf('"'))
-    $uniquenameextract = $_.innerHTML.substring($_.innerHTML.IndexOf('href="/en-us/connectors/shared_')+31)
-    $uniquename = $uniquenameextract.substring(0,$uniquenameextract.IndexOf('/'))
-    $imageurlextract = $_.innerHTML.substring($_.innerHTML.IndexOf('https://connectoricons-prod.azureedge.net/')+42)
-    $imageurl = $imageurlextract.substring(0,$imageurlextract.IndexOf('"'))
+$jsonObj = ConvertFrom-Json $([String]::new($req.Content))
+
+
+write-host "$($jsonObj.value.Count) potential connectors found. Starting processing"
+$jsonObj.value| %{
+    $name = $_.properties.displayName
+    $uniquename = $_.name -replace('shared_','')
+    $imageurl = $_.properties.iconUri
     $connectors += New-Object PSObject -Property @{
                                                 Name = $name
                                                 Uniquename = $uniquename
-                                                Url = "https://connectoricons-prod.azureedge.net/$($imageurl)"
+                                                Url = $imageurl
                                                 }
-	try{ Invoke-WebRequest "https://connectoricons-prod.azureedge.net/$($imageurl)" -OutFile "$($uniquename).png" } catch {}
+	try{ Invoke-WebRequest $imageurl -OutFile "$($uniquename).png" } catch {}
 }
 $uniqueconnectors =  $connectors | Sort-Object -Property Uniquename -Unique
 write-host "A total of $($uniqueconnectors.Count) unique connectors found"
