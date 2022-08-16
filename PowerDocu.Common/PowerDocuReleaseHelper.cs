fcf53cf58e7742e8b951bb786abf1cdaa43baa2b
Rene Modery <rene@modery.net>
2022-08-16 22:45:02 +0800

    Update PowerDocuReleaseHelper.cs
    catching exception in case it's not possible to set up a connection to github

--- PowerDocu.Common/PowerDocuReleaseHelper.cs
+++ PowerDocu.Common/PowerDocuReleaseHelper.cs
@@ -10,40 +10,47 @@ namespace PowerDocu.Common
     public static class PowerDocuReleaseHelper
     {
         public static string currentVersion = "0.9.1";
-        public static string latestVersionTag;
+        public static string latestVersionTag = currentVersion;
         public static string latestVersionUrl;
         private static bool hasReleaseBeenChecked = false;
 
         private static async Task<bool> GetLatestPowerDocuRelease()
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.UserAgent.ParseAdd(
-                "Mozilla/5.0 (compatible; PowerDocu " + currentVersion + ")"
-            );
-            var result = await client.GetAsync(
-                "https://api.github.com/repos/modery/powerdocu/releases/latest"
-            );
-            var gitHubResponseJson = await result.Content.ReadAsStringAsync();
-            JObject githubResponse = JsonConvert.DeserializeObject<JObject>(gitHubResponseJson);
-            githubResponse.TryGetValue(
-                "tag_name",
-                StringComparison.CurrentCultureIgnoreCase,
-                out JToken tagName
-            );
-            githubResponse.TryGetValue(
-                "html_url",
-                StringComparison.CurrentCultureIgnoreCase,
-                out JToken htmlUrl
-            );
-            latestVersionTag = tagName.Value<string>();
-            latestVersionUrl = htmlUrl.Value<string>();
+            try
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(
+                    "Mozilla/5.0 (compatible; PowerDocu " + currentVersion + ")"
+                );
+                var result = await client.GetAsync(
+                    "https://api.github.com/repos/modery/powerdocu/releases/latest"
+                );
+                var gitHubResponseJson = await result.Content.ReadAsStringAsync();
+                JObject githubResponse = JsonConvert.DeserializeObject<JObject>(gitHubResponseJson);
+                githubResponse.TryGetValue(
+                    "tag_name",
+                    StringComparison.CurrentCultureIgnoreCase,
+                    out JToken tagName
+                );
+                githubResponse.TryGetValue(
+                    "html_url",
+                    StringComparison.CurrentCultureIgnoreCase,
+                    out JToken htmlUrl
+                );
+                latestVersionTag = tagName.Value<string>();
+                latestVersionUrl = htmlUrl.Value<string>();
+            }
+            catch (HttpRequestException e)
+            {
+                //not doing anything here at the moment
+            }
             hasReleaseBeenChecked = true;
             return true;
         }
 
         public static async Task<bool> HasNewerPowerDocuRelease()
         {
-            if(!hasReleaseBeenChecked) await GetLatestPowerDocuRelease();
+            if (!hasReleaseBeenChecked) await GetLatestPowerDocuRelease();
             return !latestVersionTag.Contains(currentVersion);
         }
     }
