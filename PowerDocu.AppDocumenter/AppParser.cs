60b32ba0167f5119f9541150a4a69a98d9bb661e
Rene Modery <rene@modery.net>
2022-08-31 16:23:07 +0800

    Update AppParser.cs
    Adding parsing for context variables, as well as a way to check for the navigation between screens

--- PowerDocu.AppDocumenter/AppParser.cs
+++ PowerDocu.AppDocumenter/AppParser.cs
@@ -23,7 +23,7 @@ namespace PowerDocu.AppDocumenter
 
         public AppParser(string filename)
         {
-            NotificationHelper.SendNotification("Processing " + filename);
+            NotificationHelper.SendNotification(" - Processing " + filename);
             if (filename.EndsWith(".zip"))
             {
                 using (FileStream stream = new FileStream(filename, FileMode.Open))
@@ -33,8 +33,8 @@ namespace PowerDocu.AppDocumenter
                     foreach (ZipArchiveEntry definition in definitions)
                     {
                         string tempFile = Path.GetDirectoryName(filename) + @"\" + definition.Name;
-                        definition.ExtractToFile(tempFile);
-                        NotificationHelper.SendNotification("Processing app " + definition.FullName);
+                        definition.ExtractToFile(tempFile, true);
+                        NotificationHelper.SendNotification("  - Processing app " + definition.FullName);
                         using (FileStream appDefinition = new FileStream(tempFile, FileMode.Open))
                         {
                             {
@@ -53,7 +53,7 @@ namespace PowerDocu.AppDocumenter
             }
             else if (filename.EndsWith(".msapp"))
             {
-                NotificationHelper.SendNotification("Processing app " + filename);
+                NotificationHelper.SendNotification("  - Processing app " + filename);
                 packageType = PackageType.AppPackage;
                 AppEntity app = new AppEntity();
                 currentApp = app;
@@ -97,7 +97,7 @@ namespace PowerDocu.AppDocumenter
                         }
                         if (prop.Name.Equals("LogoFileName") && !String.IsNullOrEmpty(prop.Value.ToString()))
                         {
-                            ZipArchiveEntry resourceFile = ZipHelper.getFileFromZip(appArchive, "Resources\\"+prop.Value.ToString());
+                            ZipArchiveEntry resourceFile = ZipHelper.getFileFromZip(appArchive, "Resources\\" + prop.Value.ToString());
                             MemoryStream ms = new MemoryStream();
                             resourceFile.Open().CopyTo(ms);
                             currentApp.ResourceStreams.Add(prop.Value.ToString(), ms);
@@ -167,7 +167,7 @@ namespace PowerDocu.AppDocumenter
                                         break;
                                     case "InvariantScript":
                                         rule.InvariantScript = ruleProp.Value.ToString();
-                                        CheckForVariables(ruleProp.Value.ToString());
+                                        CheckForVariables(controlEntity, ruleProp.Value.ToString());
                                         break;
                                 }
                             }
@@ -200,14 +200,24 @@ namespace PowerDocu.AppDocumenter
             {
                 foreach (var globalVar in currentApp.GlobalVariables)
                 {
-                    if (rule.InvariantScript.Contains(globalVar))
+                    //if (rule.InvariantScript.Contains(globalVar))
+                    if (containsVariable(rule.InvariantScript, globalVar))
                     {
                         addVariableControlMapping(globalVar, control, rule.Property);
                     }
                 }
+                foreach (var contextVar in currentApp.ContextVariables)
+                {
+                    //if (rule.InvariantScript.Contains(contextVar))
+                    if (containsVariable(rule.InvariantScript, contextVar))
+                    {
+                        addVariableControlMapping(contextVar, control, rule.Property);
+                    }
+                }
                 foreach (var collection in currentApp.Collections)
                 {
-                    if (rule.InvariantScript.Contains(collection))
+                    // if (rule.InvariantScript.Contains(collection))
+                    if (containsVariable(rule.InvariantScript, collection))
                     {
                         addVariableControlMapping(collection, control, rule.Property);
                     }
@@ -218,6 +228,31 @@ namespace PowerDocu.AppDocumenter
                 CheckVariableUsage(child);
             }
             // TODO also check  Properties ?
+        }
+
+        private bool containsVariable(string script, string var)
+        {
+            char[] validAfterChars = { ' ', ',', ')', '.', '+', '-', '/', '*', '=', ':', '}', '\n', '\r', '&' };
+            char[] validPreChars = { ' ', '(', '+', '-', '/', '*', '=', ',', '{', '\n', '\r', '&' };
+            if (script.Contains(var))
+            {
+                if (script.StartsWith(var) || validPreChars.Contains(script[script.IndexOf(var) - 1]))
+                {
+                    if ((script.Length == (script.IndexOf(var) + var.Length)) || validAfterChars.Contains(script[script.IndexOf(var) + var.Length]))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        //not in use at the moment
+                    }
+                }
+                else
+                {
+                    //not in use at the moment
+                }
+            }
+            return false;
         }
 
         private void addVariableControlMapping(string globalVar, ControlEntity control, string property)
@@ -234,7 +269,21 @@ namespace PowerDocu.AppDocumenter
             }
         }
 
-        private void CheckForVariables(string input)
+        private void addScreenNavigation(ControlEntity controlEntity, string destinationScreen)
+        {
+            if (currentApp.ScreenNavigations.ContainsKey(controlEntity))
+            {
+                currentApp.ScreenNavigations[controlEntity].Add(destinationScreen);
+            }
+            else
+            {
+                List<string> list = new List<string>();
+                list.Add(destinationScreen);
+                currentApp.ScreenNavigations.Add(controlEntity, list);
+            }
+        }
+
+        private void CheckForVariables(ControlEntity controlEntity, string input)
         {
             //Reference: https://docs.microsoft.com/en-us/powerapps/maker/canvas-apps/working-with-variables#types-of-variables
 
@@ -249,35 +298,55 @@ namespace PowerDocu.AppDocumenter
                 }
             }
             //check for Context Variables
-
-            //Note: this is way more complex than the others. Not in use for the moment until an acceptable solution has been found
-            if (code.Contains("UpdateContext("))
+            string codeWithSpacesRemoved = code.Replace(" ", "");
+            if (codeWithSpacesRemoved.Contains("UpdateContext("))
             {
-                // Using http://regex.inginf.units.it/ , which helped to generate some of the more complex regexes. Partial success so far
-                //This captures the first instance
-                //(?<=UpdateContext\(\{)\w+
-
-                // (?<=UpdateContext\(\{)\w+|(?<=\d[^_]\w[^_],)\w+|[r-t]\w\d\w\d\w+|(?<=,)\w\w[A-Za-z]\w+(?=:\w+,)
-
-                // 37 samples. 33 correct, 4 incomplete, 0 wrong
-                // (?<=UpdateContext\(\{)\w+|(?<=[^a-i]\w[^_][^k-p][^r-v],)(?<=[^a-i][^_],)\w\w\w\w+|(?<=,)([UpdateContext]?\w[UpdateContext])+(?=:)
-
-                //46 samples. 32 correct, 12 incomplete, 2 wrong
-                // (?<=UpdateContext\(\{)\w+|(?<=\d[^}][^}][^_],)\w+|(?<=,)\w\w\w\w+(?=:\w+,)|(?<=,)(?:\w(?:\w\w)+(?=:))+(?=:"[A-Za-z])
-
-                /* if ((matches = Regex.Matches(code.Replace(" ", ""), @"(?<=UpdateContext\(\{)\w+|(?<=,)[A-Za-z]+\w+|(?<=,)\w+(?=:\d+}\))|(?<=[^a-e][^}]\d[^_],)[A-Za-z]+(?=:)")).Count > 0)
-                 {
-                     foreach (Match match in matches)
-                     {
-                         NotificationHelper.SendNotification("UC2 :" + match.Groups[0].Value);
-                         currentApp.ContextVariables.Add(match.Groups[0].Value);
-                     }
-                 }*/
+                List<int> indexes = findAllIndexesOf(codeWithSpacesRemoved, "UpdateContext(");
+                foreach (int index in indexes)
+                {
+                    foreach (string var in extractContextVariableNames(codeWithSpacesRemoved[index..]))
+                    {
+                        currentApp.ContextVariables.Add(var);
+                    }
+                }
             }
-            if (code.Contains("Navigate("))
+            if (codeWithSpacesRemoved.Contains("Navigate("))
             {
                 // As an optional third argument, pass a record that contains the context-variable name as a column name and the new value for the context variable.
-                // more complex to parse, to be implemented later
+                List<int> indexes = findAllIndexesOf(codeWithSpacesRemoved, "Navigate(");
+                foreach (int index in indexes)
+                {
+                    string firstParam = "";
+                    string secondParam = "";
+                    string thirdParam = "";
+                    string navigateString = codeWithSpacesRemoved[index..];
+                    navigateString = navigateString[0..(findClosingCharacter(navigateString, '(', ')'))].Replace("Navigate(", "");
+
+                    firstParam = extractNavigateParam(navigateString);
+                    if (firstParam != navigateString)
+                    {
+                        string navigateStringWithoutFirstParam = navigateString[(firstParam.Length + 1)..];
+                        secondParam = extractNavigateParam(navigateStringWithoutFirstParam);
+                        if (secondParam != navigateStringWithoutFirstParam)
+                        {
+                            //there's a third parameter!
+                            thirdParam = navigateStringWithoutFirstParam[(secondParam.Length + 1)..];
+                            foreach (string var in extractContextVariableNames(thirdParam))
+                            {
+                                currentApp.ContextVariables.Add(var);
+                            }
+                        }
+                        else
+                        {
+                            //only 2 parameters; nothing to do here at the moment
+                        }
+                    }
+                    else
+                    {
+                        //a single parameter only; nothing to do here at the moment
+                    }
+                    addScreenNavigation(controlEntity, firstParam);
+                }
             }
             //check for Collections
             if ((matches = Regex.Matches(code, @"\s*Collect\(\s*(?<ident>\w+)\s*,\s*")).Count > 0)
@@ -295,6 +364,155 @@ namespace PowerDocu.AppDocumenter
                     currentApp.Collections.Add(match.Groups["ident"].Value);
                 }
             }
+        }
+
+        private string extractNavigateParam(string navigateString)
+        {
+            //at least 2 commands found. We may or may not have 3 parameters in use here
+            int closingBracketIndex;
+            //find the first parameter: find the first comma (potentially after brackets)
+            char openingCharacter = 'C';
+            if (navigateString.IndexOf("(") == -1)
+            {
+                if (navigateString.IndexOf("{") > -1)
+                {
+                    openingCharacter = '{';
+                }
+            }
+            else
+            {
+                if (navigateString.IndexOf("{") > -1)
+                {
+                    if (navigateString.IndexOf("(") < navigateString.IndexOf("{"))
+                    {
+                        openingCharacter = '(';
+                    }
+                    else
+                    {
+                        openingCharacter = '{';
+                    }
+                }
+                else
+                {
+                    openingCharacter = '(';
+                }
+            }
+            if (openingCharacter == 'C' || (navigateString.IndexOf(',') < navigateString.IndexOf(openingCharacter)))
+            {
+                //no brackets for first parameter, so we can use it directly
+                if (navigateString.IndexOf(',') > -1)
+                {
+                    return navigateString[0..navigateString.IndexOf(',')];
+                }
+                //no commas at all? return it directly
+                return navigateString;
+            }
+            else
+            {
+                //brackets found. Need to find the first comma after the closing bracket
+                char closingCharacter = (openingCharacter == '(') ? ')' : '}';
+                closingBracketIndex = findClosingCharacter(navigateString, openingCharacter, closingCharacter);
+                int commaPos = navigateString.IndexOf(',', closingBracketIndex);
+                if (commaPos > -1)
+                {
+                    //comma after closing character found!
+                    return navigateString[0..commaPos];
+                }
+                else
+                {
+                    // we may end up here if there is a lot of code but no commas after 
+                    //example: "Navigate(If(2>1,Screen2,Screen2))"
+                    return navigateString;
+                }
+            }
+        }
+
+        private List<string> extractContextVariableNames(string code)
+        {
+            //todo: issue: code="If(2>1,{e:12},{e:43244})";
+            //code = "UpdateContext({Person:{Name:\"Milton\",Address:\"1MainSt\",Phone:123456789},Cat:{Name:\"Fluffy\",Age:11,Owner:{Name:\"Rene\",Phone:\"+6512334\"}}})";
+            List<string> extractedVariables = new List<string>();
+            string checkVariable = code;
+            if (code.StartsWith("UpdateContext"))
+            {
+                string variableStart = code[code.IndexOf('{')..];
+                //need to find the closing bracket for UpdateContext
+                int closingBracketIndex = findClosingCharacter(variableStart, '{', '}');
+
+                //we found the end of the current UpdateContext/Navigate. Time to extract the variable names defined here
+                checkVariable = (closingBracketIndex > 0) ? variableStart[..(closingBracketIndex + 1)] : variableStart;
+            }
+            if (checkVariable[0] == '{' && checkVariable[checkVariable.Length - 1] == '}')
+            {
+                checkVariable = checkVariable[1..(checkVariable.Length - 1)];
+            }
+
+            while (checkVariable.Contains(":"))
+            {
+                if (checkVariable.Contains('{') || checkVariable.Contains('('))
+                {
+                    int firstCurlyBracketIndex = checkVariable.IndexOf('{');
+                    int firstRoundBracketIndex = checkVariable.IndexOf('(');
+                    int firstCommaIndex = checkVariable.IndexOf(',');
+                    int closingCharacterIndex;
+                    if (firstCurlyBracketIndex > -1 && ((firstRoundBracketIndex == -1) || (firstCurlyBracketIndex < firstRoundBracketIndex)))
+                    {
+                        if ((firstCommaIndex == -1) || (firstCommaIndex > -1 && (firstCurlyBracketIndex < firstCommaIndex)))
+                        {
+                            closingCharacterIndex = findClosingCharacter(checkVariable, '{', '}');
+                        }
+                        else
+                        {
+                            closingCharacterIndex = firstCommaIndex;
+                        }
+                    }
+                    else
+                    {
+                        if ((firstCommaIndex == -1) || (firstCommaIndex > -1 && (firstRoundBracketIndex < firstCommaIndex)))
+                        {
+                            closingCharacterIndex = findClosingCharacter(checkVariable, '(', ')');
+                        }
+                        else
+                        {
+                            closingCharacterIndex = firstCommaIndex;
+                        }
+                    }
+                    extractedVariables.Add(checkVariable[0..closingCharacterIndex].Split(":")[0].Trim().Replace("{", ""));
+                    checkVariable = checkVariable[(closingCharacterIndex + ((closingCharacterIndex >= firstCommaIndex) ? 1 : 2))..];
+                    if (checkVariable.Length > 0 && checkVariable[0] == ',') checkVariable = checkVariable[1..];
+                }
+                else
+                {
+                    foreach (string variableSection in checkVariable.Split(','))
+                    {
+                        //direct assignment of variables, they can be extracted right away
+                        extractedVariables.Add(variableSection.Split(':')[0].Trim().Replace("{", ""));
+                    }
+                    checkVariable = "";
+                }
+            }
+            return extractedVariables;
+        }
+
+        private int findClosingCharacter(string content, char open, char close)
+        {
+            bool closingBracketFound = false;
+            int currentClosingBracketIndex = content.IndexOf(close);
+            if (currentClosingBracketIndex != -1)
+            {
+                while (!closingBracketFound)
+                {
+                    if (findAllIndexesOf(content[0..(currentClosingBracketIndex + 1)], open.ToString()).Count == findAllIndexesOf(content[0..(currentClosingBracketIndex + 1)], close.ToString()).Count)
+                    {
+                        closingBracketFound = true;
+                    }
+                    else
+                    {
+                        currentClosingBracketIndex = content[(currentClosingBracketIndex + 1)..].IndexOf(close) + currentClosingBracketIndex + 1;
+                    }
+                }
+            }
+            return currentClosingBracketIndex;
         }
 
         private void parseAppDataSources(Stream appArchive)
@@ -383,6 +601,18 @@ namespace PowerDocu.AppDocumenter
         public List<AppEntity> getApps()
         {
             return apps;
+        }
+
+        public List<int> findAllIndexesOf(string str, string value)
+        {
+            List<int> indexes = new List<int>();
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    return indexes;
+                indexes.Add(index);
+            }
         }
     }
 }
