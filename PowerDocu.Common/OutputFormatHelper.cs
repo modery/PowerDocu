cf2a66e6a0aaf454ffd2bda99062376f017fc227
Rene Modery <rene@modery.net>
2022-11-08 11:57:47 +0800

    Markdown Core
    Core classes to support Markdown generation

---
+++ PowerDocu.Common/OutputFormatHelper.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PowerDocu.Common
+{
+    public static class OutputFormatHelper
+    {
+        public static readonly string Word = "Word";
+        public static readonly string Markdown = "Markdown";
+        public static readonly string All = "All";
+    }
+}
