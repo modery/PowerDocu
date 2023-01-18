49f62e1a35bea3ef65785d26ef4494c1bb82c526
Rene Modery <rene@modery.net>
2023-01-18 14:51:09 +0800

    improved output (publisher, others)

--- PowerDocu.SolutionDocumenter/SolutionWordDocBuilder.cs
+++ PowerDocu.SolutionDocumenter/SolutionWordDocBuilder.cs
@@ -35,14 +35,114 @@ namespace PowerDocu.SolutionDocumenter
             Table table = CreateTable();
             table.Append(CreateRow(new Text("Status"), new Text(content.solution.isManaged ? "Managed" : "Unmanaged")));
             table.Append(CreateRow(new Text("Version"), new Text(content.solution.Version)));
-            table.Append(CreateRow(new Text("Publisher"), new Text(content.solution.Publisher.UniqueName)));
+            table.Append(CreateRow(new Text("Publisher"), GetPublisherInfo()));
+            body.Append(table);
+            para = body.AppendChild(new Paragraph());
+            run = para.AppendChild(new Run());
+            run.AppendChild(new Text("Statistics"));
+            ApplyStyleToParagraph("Heading1", para);
+            table = CreateTable();
+            table.Append(CreateHeaderRow(new Text("Component Type"), new Text("Number of Components")));
+            foreach (string componentType in content.solution.GetComponentTypes())
+            {
+                List<SolutionComponent> components = content.solution.Components.Where(c => c.Type == componentType).OrderBy(c => c.DisplayName).ToList();
+                table.Append(CreateRow(new Text(componentType), new Text(components.Count.ToString())));
+            }
             body.Append(table);
         }
 
         private Table GetPublisherInfo()
         {
             Table table = CreateTable();
-            //todo implement
+            table.Append(CreateRow(new Text("Name"), new Text(content.solution.Publisher.UniqueName)));
+            table.Append(CreateRow(new Text("Email"), new Text(content.solution.Publisher.EMailAddress)));
+            table.Append(CreateRow(new Text("CustomizationPrefix"), new Text(content.solution.Publisher.CustomizationPrefix)));
+            table.Append(CreateRow(new Text("CustomizationOptionValuePrefix"), new Text(content.solution.Publisher.CustomizationOptionValuePrefix)));
+            table.Append(CreateRow(new Text("SupportingWebsiteUrl"), new Text(content.solution.Publisher.SupportingWebsiteUrl)));
+            if (content.solution.Publisher.Descriptions.Count > 0)
+            {
+                Table descriptionsTable = CreateTable();
+                descriptionsTable.Append(CreateHeaderRow(new Text("Language Code"), new Text("Description")));
+                foreach (KeyValuePair<string, string> description in content.solution.Publisher.Descriptions)
+                {
+                    descriptionsTable.Append(CreateRow(new Text(description.Key), new Text(description.Value)));
+                }
+                table.Append(CreateRow(new Text("Descriptions"), descriptionsTable));
+            }
+            if (content.solution.Publisher.LocalizedNames.Count > 0)
+            {
+                Table localizedNamesTable = CreateTable();
+                localizedNamesTable.Append(CreateHeaderRow(new Text("Language Code"), new Text("Description")));
+                foreach (KeyValuePair<string, string> localizedName in content.solution.Publisher.LocalizedNames)
+                {
+                    localizedNamesTable.Append(CreateRow(new Text(localizedName.Key), new Text(localizedName.Value)));
+                }
+                table.Append(CreateRow(new Text("Localized Names"), localizedNamesTable));
+            }
+            //todo implement addresses
+            if (content.solution.Publisher.Addresses.Count > 0)
+            {
+                Table addressesTable = CreateTable();
+                foreach (Address address in content.solution.Publisher.Addresses)
+                {
+                    Table addressTable = CreateTable();
+                    addressTable.Append(CreateHeaderRow(new Text("Property"), new Text("Value")));
+                    if (!String.IsNullOrEmpty(address.Name))
+                        addressTable.Append(CreateRow(new Text("Name"), new Text(address.Name)));
+                    if (!String.IsNullOrEmpty(address.AddressNumber))
+                        addressTable.Append(CreateRow(new Text("AddressNumber"), new Text(address.AddressNumber)));
+                    if (!String.IsNullOrEmpty(address.AddressTypeCode))
+                        addressTable.Append(CreateRow(new Text("AddressTypeCode"), new Text(address.AddressTypeCode)));
+                    if (!String.IsNullOrEmpty(address.City))
+                        addressTable.Append(CreateRow(new Text("City"), new Text(address.City)));
+                    if (!String.IsNullOrEmpty(address.County))
+                        addressTable.Append(CreateRow(new Text("County"), new Text(address.County)));
+                    if (!String.IsNullOrEmpty(address.Country))
+                        addressTable.Append(CreateRow(new Text("Country"), new Text(address.Country)));
+                    if (!String.IsNullOrEmpty(address.Fax))
+                        addressTable.Append(CreateRow(new Text("Fax"), new Text(address.Fax)));
+                    if (!String.IsNullOrEmpty(address.FreightTermsCode))
+                        addressTable.Append(CreateRow(new Text("FreightTermsCode"), new Text(address.FreightTermsCode)));
+                    if (!String.IsNullOrEmpty(address.ImportSequenceNumber))
+                        addressTable.Append(CreateRow(new Text("ImportSequenceNumber"), new Text(address.ImportSequenceNumber)));
+                    if (!String.IsNullOrEmpty(address.Latitude))
+                        addressTable.Append(CreateRow(new Text("Latitude"), new Text(address.Latitude)));
+                    if (!String.IsNullOrEmpty(address.Line1))
+                        addressTable.Append(CreateRow(new Text("Line1"), new Text(address.Line1)));
+                    if (!String.IsNullOrEmpty(address.Line2))
+                        addressTable.Append(CreateRow(new Text("Line2"), new Text(address.Line2)));
+                    if (!String.IsNullOrEmpty(address.Line3))
+                        addressTable.Append(CreateRow(new Text("Line3"), new Text(address.Line3)));
+                    if (!String.IsNullOrEmpty(address.Longitude))
+                        addressTable.Append(CreateRow(new Text("Longitude"), new Text(address.Longitude)));
+                    if (!String.IsNullOrEmpty(address.PostalCode))
+                        addressTable.Append(CreateRow(new Text("PostalCode"), new Text(address.PostalCode)));
+                    if (!String.IsNullOrEmpty(address.PostOfficeBox))
+                        addressTable.Append(CreateRow(new Text("PostOfficeBox"), new Text(address.PostOfficeBox)));
+                    if (!String.IsNullOrEmpty(address.PrimaryContactName))
+                        addressTable.Append(CreateRow(new Text("PrimaryContactName"), new Text(address.PrimaryContactName)));
+                    if (!String.IsNullOrEmpty(address.ShippingMethodCode))
+                        addressTable.Append(CreateRow(new Text("ShippingMethodCode"), new Text(address.ShippingMethodCode)));
+                    if (!String.IsNullOrEmpty(address.StateOrProvince))
+                        addressTable.Append(CreateRow(new Text("StateOrProvince"), new Text(address.StateOrProvince)));
+                    if (!String.IsNullOrEmpty(address.Telephone1))
+                        addressTable.Append(CreateRow(new Text("Telephone1"), new Text(address.Telephone1)));
+                    if (!String.IsNullOrEmpty(address.Telephone2))
+                        addressTable.Append(CreateRow(new Text("Telephone2"), new Text(address.Telephone2)));
+                    if (!String.IsNullOrEmpty(address.Telephone3))
+                        addressTable.Append(CreateRow(new Text("Telephone3"), new Text(address.Telephone3)));
+                    if (!String.IsNullOrEmpty(address.TimeZoneRuleVersionNumber))
+                        addressTable.Append(CreateRow(new Text("TimeZoneRuleVersionNumber"), new Text(address.TimeZoneRuleVersionNumber)));
+                    if (!String.IsNullOrEmpty(address.UPSZone))
+                        addressTable.Append(CreateRow(new Text("UPSZone"), new Text(address.UPSZone)));
+                    if (!String.IsNullOrEmpty(address.UTCOffset))
+                        addressTable.Append(CreateRow(new Text("UTCOffset"), new Text(address.UTCOffset)));
+                    if (!String.IsNullOrEmpty(address.UTCConversionTimeZoneCode))
+                        addressTable.Append(CreateRow(new Text("UTCConversionTimeZoneCode"), new Text(address.UTCConversionTimeZoneCode)));
+                    addressesTable.Append(CreateRow(addressTable));
+                }
+                table.Append(CreateRow(new Text("Addresses"), addressesTable));
+            }
             return table;
         }
 
@@ -55,27 +155,53 @@ namespace PowerDocu.SolutionDocumenter
             para = body.AppendChild(new Paragraph());
             run = para.AppendChild(new Run());
             run.AppendChild(new Text("This solution contains the following components"));
-            Table table = CreateTable();
             foreach (string componentType in content.solution.GetComponentTypes())
             {
+                para = body.AppendChild(new Paragraph());
+                run = para.AppendChild(new Run());
+                run.AppendChild(new Text(componentType));
+                ApplyStyleToParagraph("Heading2", para);
                 List<SolutionComponent> components = content.solution.Components.Where(c => c.Type == componentType).OrderBy(c => c.DisplayName).ToList();
+                Table table = CreateTable();
                 table.Append(CreateHeaderRow(new Text(componentType)));
                 foreach (SolutionComponent component in components)
                 {
-                    string name = String.IsNullOrEmpty(component.SchemaName) ? component.ID : component.SchemaName;
+                    string name = "";
+                    switch (componentType)
+                    {
+                        case "Canvas App":
+                            // todo: how can we safely identify the app from here?
+                            //name = content.apps.First(a => a.ID.Equals(component.ID))?.Name;
+                            name = String.IsNullOrEmpty(component.SchemaName) ? component.ID : component.SchemaName;
+                            break;
+                        case "Workflow":
+                            name = content.flows.First(a => a.Name.ToLower().EndsWith(component.ID.ToLower().Replace("{", "").Replace("}", "")))?.Name;
+                            break;
+                        default:
+                            name = String.IsNullOrEmpty(component.SchemaName) ? component.ID : component.SchemaName;
+                            break;
+                    }
+                    name ??= String.IsNullOrEmpty(component.SchemaName) ? component.ID : component.SchemaName;
                     table.Append(CreateRow(new Text(name)));
-                    ApplyStyleToParagraph("Heading2", para);
                 }
+                body.Append(table);
+                para = body.AppendChild(new Paragraph());
+                run = para.AppendChild(new Run());
             }
-            body.Append(table);
-            para = body.AppendChild(new Paragraph());
-            run = para.AppendChild(new Run());
+
             run.AppendChild(new Text("Solution Component Dependencies"));
             ApplyStyleToParagraph("Heading1", para);
             para = body.AppendChild(new Paragraph());
             run = para.AppendChild(new Run());
             run.AppendChild(new Text("This solution has the following dependencies"));
-            foreach (string solution in content.solution.Dependencies.GroupBy(p => p.Required.Solution).Select(g => g.First()).OrderBy(t => t.Required.Solution).Select(t => t.Required.Solution).ToList())
+            foreach (string solution in content
+                                        .solution
+                                        .Dependencies
+                                        .GroupBy(p => p.Required.Solution)
+                                        .Select(g => g.First())
+                                        .OrderBy(t => t.Required.Solution)
+                                        .Select(t => t.Required.Solution)
+                                        .ToList())
             {
                 para = body.AppendChild(new Paragraph());
                 run = para.AppendChild(new Run());
@@ -83,7 +209,7 @@ namespace PowerDocu.SolutionDocumenter
                 ApplyStyleToParagraph("Heading2", para);
                 foreach (SolutionDependency dependency in content.solution.Dependencies.Where(p => p.Required.Solution.Equals(solution)))
                 {
-                    table = CreateTable();
+                    Table table = CreateTable();
                     table.Append(CreateHeaderRow(new Text("Property"), new Text("Requirement"), new Text("Dependency")));
                     if (!String.IsNullOrEmpty(dependency.Required.DisplayName) || !String.IsNullOrEmpty(dependency.Dependent.DisplayName))
                         table.Append(CreateRow(new Text("Display Name"), new Text(dependency.Required.DisplayName), new Text(dependency.Dependent.DisplayName)));
@@ -102,6 +228,8 @@ namespace PowerDocu.SolutionDocumenter
                     if (!String.IsNullOrEmpty(dependency.Required.ParentSchemaName) || !String.IsNullOrEmpty(dependency.Dependent.ParentSchemaName))
                         table.Append(CreateRow(new Text("Parent Schema Name"), new Text(dependency.Required.ParentSchemaName), new Text(dependency.Dependent.ParentSchemaName)));
                     body.Append(table);
+                    para = body.AppendChild(new Paragraph());
+                    run = para.AppendChild(new Run());
                     /*List<SolutionComponent> components = content.solution.Components.Where(c => c.Type == componentType).OrderBy(c => c.DisplayName).ToList();
                     table.Append(CreateHeaderRow(new Text(componentType)));
                     foreach (SolutionComponent component in components)
