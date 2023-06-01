922131a8ecec5b5217df3afb9d8fd0d8954c9580
Aidan Cole <aidan.cole@willmottdixon.co.uk>
2023-06-01 11:46:52 +0100

    Refactor CLI support to a common Console project. Introduce support for all variables in the front-end and add in console validation.

---
+++ PowerDocu.CLI/CommandLineOptions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using CommandLine;
+using PowerDocu.Common;
+using PowerDocu.FlowDocumenter;
+
+namespace PowerDocu.CLI
+{
+    public class CommandLineOptions
+    {
+        [Option('p', "itemsToDocument", HelpText = "Semi colon delimited list of paths of items to document", Required = false, Separator = ';')]
+        public IEnumerable<string> ItemsToDocument { get; set; }
+        [Option('m', "markDown", HelpText = "Format document as Markdown", Required = false, Default = false)]
+        public bool Markdown { get; set; } = false;
+        [Option('w', "word", HelpText = "Format document as Markdown", Required = false, Default = true)]
+        public bool Word { get; set; } = true;
+        [Option('c', "changesOnly", HelpText = "Document changes only or all properties",
+            Required = false, Default = false)]
+        public bool ChangesOnly { get; set; } = false;
+        [Option('d', "defaultValues", HelpText = "Document Canvas App Default values", Required = false, Default = true)]
+        public bool DefaultValues { get; set; } = true;
+        [Option('s', "sortFlowsByName", HelpText = "Sort flows by name", Required = false, Default = false)]
+        public bool SortFlowsByName { get; set; } = false;
+        [Option('i', "updateIcons", HelpText = "Update existing set of connector icons", Required = false, Default = false)]
+        public bool UpdateIcons { get; set; } = false;
+        [Option('t', "wordTemplate", HelpText = "Path to a word template to use when generating a word document", Required = false)]
+        public string WordTemplate { get; set; }
+
+        internal string FileFormat => this switch
+        {
+            { Word: true, Markdown: true } => OutputFormatHelper.All,
+            { Word: false, Markdown: true } => OutputFormatHelper.Markdown,
+            { Word: true, Markdown: false } => OutputFormatHelper.Word,
+            _ => OutputFormatHelper.Word
+        };
+
+        internal string SortFlowActions => this switch
+        {
+            { SortFlowsByName: true} => FlowActionSortOrderHelper.ByName,
+            { SortFlowsByName: false } => FlowActionSortOrderHelper.ByOrderOfAppearance,
+            _ => FlowActionSortOrderHelper.ByOrderOfAppearance
+        };
+    }
+}
---
+++ PowerDocu.CLI/PowerDocuCLI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandLine;
+using PowerDocu.AppDocumenter;
+using PowerDocu.Common;
+using PowerDocu.SolutionDocumenter;
+
+namespace PowerDocu.CLI
+{
+    internal class PowerDocuCLI
+    {
+        static async Task Main(string[] args)
+        {
+            NotificationHelper.AddNotificationReceiver(new ConsoleNotificationReceiver());
+
+            var options = new CommandLineOptions();
+
+            Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed(parsed =>
+            {
+                options = parsed;
+            });
+
+            switch (options.UpdateIcons)
+            {
+                case true:
+                    await ConnectorHelper.UpdateConnectorIcons();
+                    break;
+                case false when !options.ItemsToDocument.Any():
+                    NotificationHelper.SendNotification($"No items to generate documentation on provided");
+                    break;
+                case false when !options.ItemsToDocument.All(itemToDocument => new List<string> { ".zip", ".msapp" }.Contains(Path.GetExtension(itemToDocument))):
+                    NotificationHelper.SendNotification($"No valid file provided, valid files are either .zip or .msapp formats");
+                    break;
+                case false when options.Word && !string.IsNullOrEmpty(options.WordTemplate) && !new List<string> { ".docx", ".docm", ".dtox"}.Contains(Path.GetExtension(options.WordTemplate)):
+                    NotificationHelper.SendNotification($"An invalid word document was provided as the Word Template, expected the file to be .docx, .docm or .dotx format");
+                    break;
+                default:
+                    GenerateDocumentation(options);
+                    break;
+            }
+        }
+
+        private static void GenerateDocumentation(CommandLineOptions options)
+        {
+            foreach (var itemToDocument in options.ItemsToDocument)
+            {
+                switch (Path.GetExtension(itemToDocument))
+                {
+                    case ".zip":
+                        SolutionDocumentationGenerator.GenerateDocumentation(itemToDocument, options.FileFormat, options.ChangesOnly, options.DefaultValues, options.SortFlowActions, options.WordTemplate);
+                        break;
+                    case ".msapp":
+                        AppDocumentationGenerator.GenerateDocumentation(itemToDocument, options.FileFormat,
+                            options.ChangesOnly, options.DefaultValues, options.WordTemplate);
+                        break;
+                }
+            }
+        }
+    }
+}
