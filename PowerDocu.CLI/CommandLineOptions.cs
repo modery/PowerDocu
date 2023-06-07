86e73ff8d6998be816c953e0d0665201a4e916be
Aidan Cole <aidan.cole@willmottdixon.co.uk>
2023-06-07 16:12:06 +0100

    Correction to CLI options

--- PowerDocu.CLI/CommandLineOptions.cs
+++ PowerDocu.CLI/CommandLineOptions.cs
@@ -1,30 +1,27 @@
 ﻿using System.Collections.Generic;
-using System.Linq.Expressions;
 using CommandLine;
 using PowerDocu.Common;
-using PowerDocu.FlowDocumenter;
 
 namespace PowerDocu.CLI
 {
     public class CommandLineOptions
     {
         [Option('p', "itemsToDocument", HelpText = "Semi colon delimited list of paths of items to document", Required = false, Separator = ';')]
-        public IEnumerable<string> ItemsToDocument { get; set; }
-        [Option('m', "markDown", HelpText = "Format document as Markdown", Required = false, Default = false)]
-        public bool Markdown { get; set; } = false;
-        [Option('w', "word", HelpText = "Format document as Markdown", Required = false, Default = true)]
-        public bool Word { get; set; } = true;
-        [Option('c', "changesOnly", HelpText = "Document changes only or all properties",
-            Required = false, Default = false)]
-        public bool ChangesOnly { get; set; } = false;
-        [Option('d', "defaultValues", HelpText = "Document Canvas App Default values", Required = false, Default = true)]
-        public bool DefaultValues { get; set; } = true;
-        [Option('s', "sortFlowsByName", HelpText = "Sort flows by name", Required = false, Default = false)]
-        public bool SortFlowsByName { get; set; } = false;
-        [Option('i', "updateIcons", HelpText = "Update existing set of connector icons", Required = false, Default = false)]
-        public bool UpdateIcons { get; set; } = false;
+        public IEnumerable<string>? ItemsToDocument { get; set; }
+        [Option('m', "markDown", HelpText = "Format document as Markdown", Required = false)]
+        public bool Markdown { get; set; }
+        [Option('w', "word", HelpText = "Format document as Word", Required = false)]
+        public bool Word { get; set; }
+        [Option('c', "changesOnly", HelpText = "Document changes only or all properties", Required = false)]
+        public bool ChangesOnly { get; set; }
+        [Option('d', "defaultValues", HelpText = "Document Canvas App Default values", Required = false)]
+        public bool DefaultValues { get; set; }
+        [Option('s', "sortFlowsByName", HelpText = "Sort flows by name", Required = false)]
+        public bool SortFlowsByName { get; set; }
+        [Option('i', "updateIcons", HelpText = "Update existing set of connector icons", Required = false)]
+        public bool UpdateIcons { get; set; }
         [Option('t', "wordTemplate", HelpText = "Path to a word template to use when generating a word document", Required = false)]
-        public string WordTemplate { get; set; }
+        public string? WordTemplate { get; set; }
 
         internal string FileFormat => this switch
         {
